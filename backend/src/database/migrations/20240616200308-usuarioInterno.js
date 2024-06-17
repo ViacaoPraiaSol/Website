58072f8bee75737c55f227fb12b93c41b5bcf363
Arthur Torres <54257895+NamelessMiojo@users.noreply.github.com>
2024-06-17 12:39:02 -0300

    v0.04

--- backend/src/database/migrations/20240616200308-usuarioInterno.js
+++ backend/src/database/migrations/20240616200308-usuarioInterno.js
@@ -3,7 +3,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await queryInterface.createTable('usuario', {
+    await queryInterface.createTable('usuario_interno', {
       id: {
         type: Sequelize.UUID,
         primaryKey: true,
@@ -35,6 +35,6 @@ module.exports = {
   },
 
   async down (queryInterface, Sequelize) {
-    await queryInterface.dropTable('usuario')
+    await queryInterface.dropTable('usuario_interno')
   }
 };
