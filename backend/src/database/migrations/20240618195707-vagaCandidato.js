51977e1eea2536baac95aecc6f2cdbd93423d7fe
TI-PraiaSol <suporte.ti@viacaopraiasol.com.br>
2024-06-19 17:40:32 -0300

    Update 20240618195707-vagaCandidato.js

--- backend/src/database/migrations/20240618195707-vagaCandidato.js
+++ backend/src/database/migrations/20240618195707-vagaCandidato.js
@@ -3,15 +3,27 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable('vaga_candidato', {
+      vaga_id:{
+        type: Sequelize.UUID,
+        allowNull: false,
+        references: {
+          model: 'vaga',
+          key: 'id'
+        }
+      },
+      usuario_externo_id:{
+        type: Sequelize.UUID,
+        allowNull: false,
+        references: {
+          model: 'usuario_externo',
+          key: 'id'
+        }
+      },
+    })
   },
 
   async down (queryInterface, Sequelize) {
-    await queryInterface.dropTable('vaga_publicada')
+    await queryInterface.dropTable('vaga_candidato')
   }
 };
